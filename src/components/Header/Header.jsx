6e95b725f8519c28cd81bb6c950f312c885e21fa
Grace-3908254 <choore.11@gmail.com>
2024-10-08 15:06:58 -0400

    add background color to header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Header.scss';
+import logo from '../../assets/Logo/InStock-Logo_2x.png'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        <img className='Header__logo' src={logo} alt='logo at header'></img>
+        <div className='Header__nav'>
+            <button className='Header__warehouses Header__wareghouses--active Header__button'>Warehouses</button>
+            <button className='Header__inventory Header__inventory--active Header__button'>Inventory</button>
+        </div>
+    </div>
   )
 }
