43c46f7ebef0f3d52e248326ceb33c90f02f4264
himanidesai <de99hi@gmail.com>
2024-10-09 13:57:09 -0400

    Adding default redirect page to get the warehousePage link in url

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Warehouse from './pages/Warehouse/Warehouse'
 import Inventory from './pages/Inventory/Inventory'
 import Footer from './components/Footer/Footer'
+import Redirect from './components/Redirect/Redirect'
 
 function App() {
 
@@ -10,7 +11,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/*' element={<Warehouse/>}></Route>
+          <Route path='/*' element={<Redirect/>}></Route>
           <Route path='/warehousePage/*' element={<Warehouse/>}></Route>
           <Route path='/inventoryPage/*' element={<Inventory/>}></Route>
         </Routes>
