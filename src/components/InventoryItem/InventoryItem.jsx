e869f366ab83e3344fad6289d065605d6223f992
himanidesai <de99hi@gmail.com>
2024-10-09 15:21:03 -0400

    Stashing my basic changes into the feature for storing my work

---
+++ src/components/InventoryItem/InventoryItem.jsx
@@ -0,0 +1,60 @@
+// import React from 'react';
+// import './InventoryItem.scss';
+// import { Link, useNavigate } from 'react-router-dom';
+// import editIcon from '../../assets/icons/edit-24px.svg';
+// import deleteicon from '../../assets/icons/delete_outline-24px.svg';
+// import ChevronRightIcon from '../../assets/icons/chevron_right-24px.svg';
+
+
+// function InventoryItem({inventory}) {
+//     const statusClass = inventory.status.toLowerCase() === 'in stock' ? 'in-stock' : 'out-of-stock';
+//     const navigate = useNavigate();
+
+//     return (
+//         <div className="inventory-item">
+//             <div className="inventory-item__main-container">
+//                 <div className="inventory-item__details-container">
+//                     <div className="inventory-item__name-container">
+//                         <p className="inventory-item__label-mobile">INVENTORY ITEM</p>
+//                         <Link to={`/inventory/${inventory.id}`} className="inventory-item__name-sub-container">
+//                             <p className="inventory-item__name">{inventory.item_name}</p>
+//                             <img src={ChevronRightIcon} alt="More details" className="inventory-item__chevron" />
+//                         </Link>
+//                     </div>
+//                     <div className="inventory-item__category-container">
+//                         <p className="inventory-item__label-mobile">CATEGORY</p>
+//                         <p className="inventory-item__category">{inventory.category}</p>
+//                     </div>
+//                 </div>
+//                 <div className="inventory-item__info-container">
+//                     <div className="inventory-item__status-container">
+//                         <p className="inventory-item__label-mobile">STATUS</p>
+//                         <p className={`inventory-item__status ${statusClass}`}>{inventory.status}</p>
+//                     </div>
+//                     <div className="inventory-item__quantity-container">
+//                         <p className="inventory-item__label-mobile">QTY</p>
+//                         <p className="inventory-item__quantity">{inventory.quantity}</p>
+//                     </div>
+//                     <div className="inventory-item__warehouse-container inventory-item__warehouse-container-warehouse">
+//                         <p className="inventory-item__label-mobile">WAREHOUSE</p>
+//                         <p className="inventory-item__warehouse">{inventory.warehouse_name}</p>
+//                     </div>
+//                 </div>
+//             </div>
+//             <div className="inventory-item__actions">
+// 							<div className='inventory-item__actions-container'>
+//                 <button aria-label="Delete item" className="inventory-item__delete">
+//                     <img src={deleteicon} id={inventory.id} />
+//                 </button>
+// 							</div>
+// 							<div className='inventory-item__actions-container'>
+//                 <button aria-label="Delete item" className="inventory-item__delete">
+//                     <img src={editIcon} onClick={(()=> navigate(`/inventoryPage/inventory/edit/${inventory.id}`))} />
+//                 </button>
+// 							</div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default InventoryItem;
--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -1,9 +1,44 @@
 import './InventoryList.scss';
+import SortIcon from '../../assets/Icons/sort-24px.svg'
 
-function InventoryList () {
+function InventoryList ({inventories}) {
     return (
         <>
-        <div>In the inventory list</div>
+        <section className="inventory-list-section">
+            <div className="inventory-list-section__labels">
+								<div className="inventory-list-section__label-container inventory-list-section__label-container-inventory">
+									<div className="inventory-list-section__label">INVENTORY ITEM</div> 
+									<img src={SortIcon} alt="Sort" className="inventory-list-section__label-img" />
+								</div>
+								<div className="inventory-list-section__label-container inventory-list-section__label-container-category">
+									<div className="inventory-list-section__label">CATEGORY</div> 
+									<img src={SortIcon} alt="Sort" className="inventory-list-section__label-img" />
+								</div>
+                <div className="inventory-list-section__label-container inventory-list-section__label-container-status">
+									<div className="inventory-list-section__label">STATUS</div>
+									<img src={SortIcon} alt="Sort" className="inventory-list-section__label-img"/>
+                </div>
+                <div className="inventory-list-section__label-container inventory-list-section__label-container-quantity">
+									<div className="inventory-list-section__label">QTY</div>
+									<img src={SortIcon} alt="Sort" className="inventory-list-section__label-img"/>
+                </div>
+                <div className="inventory-list-section__label-container inventory-list-section__label-container-warehouse">
+									<div className="inventory-list-section__label inventory-list-section__label-warehouse">WAREHOUSE</div>
+									<img src={SortIcon} alt="Sort" className="inventory-list-section__label-img"/>
+                </div>
+                <div className="inventory-list-section__label-container">
+									<div className="inventory-list-section__label">ACTIONS</div>
+                </div>
+            </div>
+            <ul className="inventory-list">
+                {inventories.map((item) => (
+                    <InventoryItem 
+                        key={item.id} 
+                        inventory={item}
+                    />
+                ))}
+            </ul>           
+        </section>
         </>
     )
 }
