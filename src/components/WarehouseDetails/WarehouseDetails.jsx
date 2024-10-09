76c9c293999326d9ff39ea2e8f41ed6815cfdf51
Vina Quiambao <mariavinaq@gmail.com>
2024-10-09 18:47:51 -0400

    Complete styling of WarehouseDetails component  for all breakpoints

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -10,21 +10,23 @@ function WarehouseDetails () {
                 <h1>Washington</h1>
                 <button className='warehouse-details__edit-button'>
                     <img className='warehouse-details__edit-button-icon' src={iconEdit} />
-                    
+                    <span className='warehouse-details__edit-button-label'>Edit</span>
                 </button>
             </div>
             <div className='warehouse-details__about'>
                 <div className='warehouse-details__address'>
-                    <p className='warehouse-details__label'>Warehouse Address:</p>
-                    <p className='warehouse-details__data'>33 Pearl Street SW, Washington, USA</p>
+                    <div className='warehouse-details__column warehouse-details__column--address'>
+                        <p className='warehouse-details__label'>Warehouse Address:</p>
+                        <p className='warehouse-details__data'>33 Pearl Street SW, Washington, USA</p>
+                    </div>
                 </div>
                 <div className='warehouse-details__contact'>
-                    <div className='warehouse-details__contact-column'>
+                    <div className='warehouse-details__column'>
                         <p className='warehouse-details__label'>Contact Name:</p>
                         <p className='warehouse-details__data'>Graeme Lyon</p>
                         <p className='warehouse-details__data'>Warehouse Manager</p>
                     </div>
-                    <div className='warehouse-details__contact-column'>
+                    <div className='warehouse-details__column'>
                         <p className='warehouse-details__label'>Contact Information:</p>
                         <p className='warehouse-details__data'>+1 (647) 504-0911</p>
                         <p className='warehouse-details__data'>glyon@instock.com</p>
