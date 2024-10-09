6c2c17a909a3160cd421a61d55140cb11c2e61f1
himanidesai <de99hi@gmail.com>
2024-10-09 15:30:09 -0400

    Simplifying the routing into single App.jsx file

--- src/components/Redirect/Redirect.jsx
+++ src/components/Redirect/Redirect.jsx
@@ -5,7 +5,7 @@ export default function Redirect () {
     const navigate = useNavigate();
     
     useEffect(()=>{
-        navigate('/warehousePage');
+        navigate('/warehouses');
     }, [])
     
     return <></>
