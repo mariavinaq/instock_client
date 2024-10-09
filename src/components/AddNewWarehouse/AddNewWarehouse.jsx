086900e58acd519285d3c662e38dfa5e0dd7223d
Grace-3908254 <choore.11@gmail.com>
2024-10-09 19:03:10 -0400

    add functionality to send new warehouse info

--- src/components/AddNewWarehouse/AddNewWarehouse.jsx
+++ src/components/AddNewWarehouse/AddNewWarehouse.jsx
@@ -1,9 +1,30 @@
 import './AddNewWarehouse.scss';
 
 function AddNewWarehouse () {
+
+    
+    //this is the handleSubmit function for the form 
+    async function postWarehouse(event) {
+        event.preventDefault();
+        const newWarehouse = {
+            warehouse_name: event.target.warehouse_name.value, 
+            address: event.target.address.value, 
+            city: event.target.city.value, 
+            country: event.target.country.value, 
+            contact_name: event.target.contact_name.value, 
+            contact_position: event.target.contact_position.value, 
+            contact_phone: event.target.contact_phone.value, 
+            contact_email: event.target.contact_email.value
+        }
+
+       const response = await axios.post("http://localhost:8080/warehouses", newWarehouse)
+    }
+
     return (
         <>
-        <div>In the Add new warehouse</div>
+            <form onSubmit={postWarehouse}>
+
+            </form>
         </>
     )
 }
