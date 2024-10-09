dd2f5789a08998df623eea743aa1a3b55a89cc1c
komyelizqa <yelizavkom@gmail.com>
2024-10-08 19:58:49 -0700

    correct styles of header and footer according to mockup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './Header.scss';
 import logo from '../../assets/Logo/InStock-Logo_2x.png'
@@ -20,16 +19,3 @@ export default function Header() {
   )
 }
 
-//dive outside of button is reserved for <Link> tags later
-=======
-import './Header.scss';
-
-function Header () {
-    return (
-        <>
-        </>
-    )
-}
-
-export default Header;
->>>>>>> develop
