634f7fc0813704876456730f6e86dee4a964ad37
Grace-3908254 <choore.11@gmail.com>
2024-10-09 14:42:45 -0400

    modify Header component: sizes more closely resemble to Figma with one discrepancy (Header.scss, line 27 to match the logo with the nav buttons and to move it to the center); add links and discern the button style when being clicked on

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Warehouse from './pages/Warehouse/Warehouse'
 import Inventory from './pages/Inventory/Inventory'
