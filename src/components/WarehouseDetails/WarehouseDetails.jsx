c22a2b863ae31dde301dcf0e184d3a2f6b27acba
Vina Quiambao <mariavinaq@gmail.com>
2024-10-10 15:07:30 -0400

    Save for rebase

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,12 +1,16 @@
+import { Link } from 'react-router-dom';
 import iconArrowBack from '../../assets/Icons/arrow_back-24px.svg'
 import iconEdit from '../../assets/Icons/edit-white-24px.svg'
+import Warehouse from '../../pages/Warehouse/Warehouse';
 import './WarehouseDetails.scss';
 
 function WarehouseDetails () {
     return (
         <div className='warehouse-details'>
             <div className='warehouse-details__header'>
-                <img className='warehouse-details__back' src={iconArrowBack} />
+                <Link to='/warehouses' element={<Warehouse />}>
+                    <img className='warehouse-details__back' src={iconArrowBack} />
+                </Link>
                 <h1>Washington</h1>
                 <button className='warehouse-details__edit-button'>
                     <img className='warehouse-details__edit-button-icon' src={iconEdit} />
