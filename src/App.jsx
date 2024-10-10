bdfc5d10b5acb61d97553e7ac5753e7a9b6d179d
Okashe <okashabdi88@gmail.com>
2024-10-10 10:27:19 -0500

    Complete warehouse list page

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,55 @@
-import './App.scss'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Warehouse from './pages/Warehouse/Warehouse'
-import Inventory from './pages/Inventory/Inventory'
-import Footer from './components/Footer/Footer'
-import Redirect from './components/Redirect/Redirect'
-import InventoryItemDetails from './components/InventoryItemDetails/InventoryItemDetails'
-import EditInventoryItem from './components/EditInventoryItem/EditInventoryItem'
-import AddNewInventory from './components/AddNewInventory/AddNewInventory'
-import Header from './components/Header/Header'
-import EditWarehouseItem from './components/EditWarehouseItem/EditWarehouseItem'
-import AddNewWarehouse from './components/AddNewWarehouse/AddNewWarehouse'
-import WarehouseItemDetails from './components/WarehouseItemDetails/WarehouseItemDetails'
-import ContentWrapper from './components/ContentWrapper/ContentWrapper'
+import "./App.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Warehouse from "./pages/Warehouse/Warehouse";
+import Inventory from "./pages/Inventory/Inventory";
+import Footer from "./components/Footer/Footer";
+import Redirect from "./components/Redirect/Redirect";
+import InventoryItemDetails from "./components/InventoryItemDetails/InventoryItemDetails";
+import EditInventoryItem from "./components/EditInventoryItem/EditInventoryItem";
+import AddNewInventory from "./components/AddNewInventory/AddNewInventory";
+import Header from "./components/Header/Header";
+import EditWarehouseItem from "./components/EditWarehouseItem/EditWarehouseItem";
+import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
+import WarehouseItemDetails from "./components/WarehouseItemDetails/WarehouseItemDetails";
+import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <ContentWrapper>
           <Routes>
-            <Route path='/' element={<Redirect/>}></Route>
-            <Route path='/warehouses' element={<Warehouse/>}></Route>
-            <Route path='/warehouses/add' element={<AddNewWarehouse/>}></Route>
-            <Route path='/warehouses/edit/:id' element={<EditWarehouseItem/>}></Route>
-            <Route path='/warehouses/:id' element={<WarehouseItemDetails/>}></Route>
-            <Route path='/inventories' element={<Inventory/>}></Route>
-            <Route path='/inventories/add' element={<AddNewInventory/>}></Route>
-            <Route path='/inventories/edit/:id' element={<EditInventoryItem/>}></Route>
-            <Route path='/inventories/:id' element={<InventoryItemDetails/>}></Route>
+            <Route path="/" element={<Redirect />}></Route>
+            <Route path="/warehouses" element={<Warehouse />}></Route>
+            <Route path="/warehouses/add" element={<AddNewWarehouse />}></Route>
+            <Route
+              path="/warehouses/edit/:id"
+              element={<EditWarehouseItem />}
+            ></Route>
+            <Route
+              path="/warehouses/:id"
+              element={<WarehouseItemDetails />}
+            ></Route>
+            <Route path="/inventories" element={<Inventory />}></Route>
+            <Route
+              path="/inventories/add"
+              element={<AddNewInventory />}
+            ></Route>
+            <Route
+              path="/inventories/edit/:id"
+              element={<EditInventoryItem />}
+            ></Route>
+            <Route
+              path="/inventories/:id"
+              element={<InventoryItemDetails />}
+            ></Route>
           </Routes>
         </ContentWrapper>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
