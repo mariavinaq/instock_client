a2e6257af61ea54f597dd55719779abbe7206ff2
Grace-3908254 <choore.11@gmail.com>
2024-10-11 15:58:56 -0400

    style component

--- src/components/EditWarehouseItem/EditWarehouseItem.jsx
+++ src/components/EditWarehouseItem/EditWarehouseItem.jsx
@@ -2,55 +2,55 @@ import './EditWarehouseItem.scss';
 
 function EditWarehouseItem () {
     return (
-        <div className='addWarehouse'>
-            <div className='addWarehouse__empty'></div>
-            <div className='addWarehouse__header'>
+        <div className='editWarehouse'>
+            <div className='editWarehouse__empty'></div>
+            <div className='editWarehouse__header'>
                 <Link to={"/"}>
-                    <img className='addWarehouse__header-icon' src={arrowBack} alt='arrowBack'></img>    
+                    <img className='editWarehouse__header-icon' src={arrowBack} alt='arrowBack'></img>    
                 </Link>
                 
-                <h1 className='addWarehouse__header-itle'>Add New Warehouse</h1>
+                <h1 className='editWarehouse__header-itle'>Add New Warehouse</h1>
             </div>
-            <div className='addWarehouse__formWrapper'>
-               <form onSubmit={postWarehouse} className='addwarehouse__form'>
+            <div className='editWarehouse__formWrapper'>
+               <form onSubmit={postWarehouse} className='editWarehouse__form'>
 
-                <div className='addWarehouse__form-sections'>
-                   <div className='addWarehouse__form-section addWarehouse__form-warehouse'>
-                    <h2 className='addWarehouse__form-details'>Warehouse Details</h2>
+                <div className='editWarehouse__form-sections'>
+                   <div className='editWarehouse__form-section editWarehouse__form-warehouse'>
+                    <h2 className='editWarehouse__form-details'>Warehouse Details</h2>
 
-                    <label className='addWarehouse__form-label' htmlFor='warehouse_name'>Warehouse Name</label>
-                    <input className='addWarehouse__form-input' name='warehouse_name' id='warehouse_name' placeholder='Warehouse Name'></input>
+                    <label className='editWarehouse__form-label' htmlFor='warehouse_name'>Warehouse Name</label>
+                    <input className='editWarehouse__form-input' name='warehouse_name' id='warehouse_name' placeholder='Warehouse Name'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='address'>Street Name</label>
-                    <input className='addWarehouse__form-input' name='address' id='address' placeholder='Street Name'></input>
+                    <label className='editWarehouse__form-label' htmlFor='address'>Street Name</label>
+                    <input className='editWarehouse__form-input' name='address' id='address' placeholder='Street Name'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='city'>City</label>
-                    <input className='addWarehouse__form-input' name='city' id='city' placeholder='City'></input>
+                    <label className='editWarehouse__form-label' htmlFor='city'>City</label>
+                    <input className='editWarehouse__form-input' name='city' id='city' placeholder='City'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='country'>Country</label>
-                    <input className='addWarehouse__form-input' name='country' id='country' placeholder='Country'></input>
+                    <label className='editWarehouse__form-label' htmlFor='country'>Country</label>
+                    <input className='editWarehouse__form-input' name='country' id='country' placeholder='Country'></input>
                     </div>
 
-                    <div className='addWarehouse__form-section addWarehouse__form-contactDetails'>
-                    <h2 className='addWarehouse__form-details'>Contact Details</h2>
+                    <div className='editWarehouse__form-section editWarehouse__form-contactDetails'>
+                    <h2 className='editWarehouse__form-details'>Contact Details</h2>
                     
-                    <label className='addWarehouse__form-label' htmlFor='contact_name'>Contact Name</label>
-                    <input className='addWarehouse__form-input' name='contact_name' id='contact_name' placeholder='Contact Name'></input>
+                    <label className='editWarehouse__form-label' htmlFor='contact_name'>Contact Name</label>
+                    <input className='editWarehouse__form-input' name='contact_name' id='contact_name' placeholder='Contact Name'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='contact_position'>Position</label>
-                    <input className='addWarehouse__form-input' name='contact_position' id='contact_position' placeholder='Position'></input>
+                    <label className='editWarehouse__form-label' htmlFor='contact_position'>Position</label>
+                    <input className='editWarehouse__form-input' name='contact_position' id='contact_position' placeholder='Position'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='contact_phone'>Phone Number</label>
-                    <input className='addWarehouse__form-input' name='contact_phone' id='contact_phone' placeholder='Phone Number'></input>
+                    <label className='editWarehouse__form-label' htmlFor='contact_phone'>Phone Number</label>
+                    <input className='editWarehouse__form-input' name='contact_phone' id='contact_phone' placeholder='Phone Number'></input>
 
-                    <label className='addWarehouse__form-label' htmlFor='contact_email'>Email</label>
-                    <input className='addWarehouse__form-input' name='contact_email' id='contact_email' placeholder='Email'></input>
+                    <label className='editWarehouse__form-label' htmlFor='contact_email'>Email</label>
+                    <input className='editWarehouse__form-input' name='contact_email' id='contact_email' placeholder='Email'></input>
                     </div> 
                 </div>
                 
-                <div className='addWarehouse__form-buttons'>
-                    <button className='addWarehouse__form-button addWarehouse__form-cancel' type="button" onClick={handleReset}>Cancel</button>
-                    <button className='addWarehouse__form-button addWarehouse__form-add' type='submit'>+ Add Warehouse</button>
+                <div className='editWarehouse__form-buttons'>
+                    <button className='editWarehouse__form-button editWarehouse__form-cancel' type="button" onClick={handleReset}>Cancel</button>
+                    <button className='editWarehouse__form-button editWarehouse__form-add' type='submit'>+ Add Warehouse</button>
                 </div>
                 </form> 
             </div>
