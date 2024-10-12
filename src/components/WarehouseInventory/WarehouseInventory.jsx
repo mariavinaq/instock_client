9867e98bf477e6a66a30a57facb776d8eccf2d99
Vina Quiambao <mariavinaq@gmail.com>
2024-10-11 21:05:29 -0400

    Incorporate full button and api functionaiities for warehouse-specific inventory

--- src/components/WarehouseInventory/WarehouseInventory.jsx
+++ src/components/WarehouseInventory/WarehouseInventory.jsx
@@ -2,34 +2,32 @@ import './WarehouseInventory.scss';
 import SortIcon from '../../assets/Icons/sort-24px.svg'
 import WarehouseInventoryItem from '../WarehouseInventoryItem/WarehouseInventoryItem';
 
-function WarehouseInventory() {
-// function WarehouseInventory({ inventory }) {
+function WarehouseInventory({ inventory, handleOpenInventoryModal }) {
     return (
         <section className="warehouse-inventory">
             <div className="warehouse-inventory__labels">
                 <div className="warehouse-inventory__label-container warehouse-inventory__label-container--inventory">
-                    <div className="warehouse-inventory__label">INVENTORY ITEM</div>
+                    <div className="warehouse-inventory__label">Inventory Item</div>
                     <img src={SortIcon} alt="Sort" className="warehouse-inventory__sort" />
                 </div>
                 <div className="warehouse-inventory__label-container warehouse-inventory__label-container--category">
-                    <div className="warehouse-inventory__label">CATEGORY</div>
+                    <div className="warehouse-inventory__label">Category</div>
                     <img src={SortIcon} alt="Sort" className="warehouse-inventory__sort" />
                 </div>
                 <div className="warehouse-inventory__label-container warehouse-inventory__label-container--status">
-                    <div className="warehouse-inventory__label">STATUS</div>
+                    <div className="warehouse-inventory__label">Status</div>
                     <img src={SortIcon} alt="Sort" className="warehouse-inventory__sort" />
                 </div>
                 <div className="warehouse-inventory__label-container warehouse-inventory__label-container--quantity">
-                    <div className="warehouse-inventory__label">QUANTITY</div>
+                    <div className="warehouse-inventory__label">Quantity</div>
                     <img src={SortIcon} alt="Sort" className="warehouse-inventory__sort" />
                 </div>
                 <div className="warehouse-inventory__label-container warehouse-inventory__label-container--actions">
-                    <div className="warehouse-inventory__label">ACTIONS</div>
+                    <div className="warehouse-inventory__label">Actions</div>
                 </div>
             </div>
             <ul className="warehouse-inventory__items">
-                    {/* {inventory.map(item => <WarehouseInventoryItem key={item.id} item={item} />)} */}
-                    <WarehouseInventoryItem />
+                    {inventory.map(item => <WarehouseInventoryItem key={item.id} item={item} handleOpenInventoryModal={handleOpenInventoryModal} />)}
                 </ul>
         </section>
     );
--- src/components/WarehouseInventoryItem/WarehouseInventoryItem.jsx
+++ src/components/WarehouseInventoryItem/WarehouseInventoryItem.jsx
@@ -4,9 +4,8 @@ import deleteicon from '../../assets/icons/delete_outline-24px.svg';
 import ChevronRightIcon from '../../assets/icons/chevron_right-24px.svg';
 import './WarehouseInventoryItem.scss';
 
-// function WarehouseInventoryItem({ item }) {
-function WarehouseInventoryItem() {
-    // const statusClass = inventory.status.toLowerCase() === 'in stock' ? 'in-stock' : 'out-of-stock';
+function WarehouseInventoryItem({ item, handleOpenInventoryModal }) {
+    const statusClass = item.status.toLowerCase() === 'in stock' ? 'in-stock' : 'out-of-stock';
     const navigate = useNavigate();
 
     return (
@@ -14,39 +13,34 @@ function WarehouseInventoryItem() {
             <div className="warehouse-item__main-container">
                 <div className="warehouse-item__details-container">
                     <div className="warehouse-item__name-container">
-                        <p className="warehouse-item__label-mobile">INVENTORY ITEM</p>
-                        {/* <Link to={`/inventories/${item.id}`} className="warehouse-item__name-sub-container"> */}
-                        <Link to={`/inventories`} className="warehouse-item__name-sub-container">
-                            {/* <p className="warehouse-item__name">{item.item_name}</p> */}
-                            <p className="warehouse-item__name">Placeholder</p>
+                        <p className="warehouse-item__label-mobile">Inventory Item</p>
+                        <Link to={`/inventories/${item.id}`} className="warehouse-item__name-sub-container">
+                            <p className="warehouse-item__name">{item.item_name}</p>
                             <img src={ChevronRightIcon} alt="More details" className="warehouse-item__chevron" />
                         </Link>
                     </div>
                     <div className="warehouse-item__category-container warehouse-item__category-container">
-                        <p className="warehouse-item__label-mobile">CATEGORY</p>
-                        {/* <p className="warehouse-item__category">{item.category}</p> */}
-                        <p className="warehouse-item__category">Placeholder</p>
+                        <p className="warehouse-item__label-mobile">Category</p>
+                        <p className="warehouse-item__category">{item.category}</p>
                     </div>
                 </div>
                 <div className="warehouse-item__info-container">
                     <div className="warehouse-item__status-container">
-                        <p className="warehouse-item__label-mobile">STATUS</p>
-                        {/* <p className={`warehouse-item__status ${statusClass}`}>{item.status}</p> */}
-                        <p className={`warehouse-item__status in-stock`}>Placeholder</p>
+                        <p className="warehouse-item__label-mobile">Status</p>
+                        <p className={`warehouse-item__status ${statusClass}`}>{item.status}</p>
                     </div>
                     <div className="warehouse-item__quantity-container">
-                        <p className="warehouse-item__label-mobile">QTY</p>
-                        {/* <p className="warehouse-item__quantity">{item.quantity}</p> */}
-                        <p className="warehouse-item__quantity">Placeholder</p>
+                        <p className="warehouse-item__label-mobile">Qty</p>
+                        <p className="warehouse-item__quantity">{item.quantity}</p>
                     </div>
                 </div>
             </div>
             <div className="warehouse-item__actions">
                     <button aria-label="Delete item" className="warehouse-item__delete">
-                        <img src={deleteicon}/>
+                        <img src={deleteicon} id={item.id} onClick={handleOpenInventoryModal}/>
                     </button>
                     <button aria-label="edit item" className="warehouse-item__edit">
-                        <img src={editIcon} onClick={(() => navigate(`/warehouses/edit/:id`))} />
+                        <img src={editIcon} onClick={(() => navigate(`/inventories/edit/${item.id}`))} />
                     </button>
             </div>
         </div>
