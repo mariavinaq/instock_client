ae9573fd1d31f0cdeeed08a43f82355f5d36f080
Okashe <okashabdi88@gmail.com>
2024-10-14 16:57:13 -0500

    update cursor effect & replace div with h1 and p

--- src/components/InventoryModal/InventoryModal.jsx
+++ src/components/InventoryModal/InventoryModal.jsx
@@ -1,47 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './InventoryModal.scss';
-import Modal from '../Modal/Modal';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./InventoryModal.scss";
+import Modal from "../Modal/Modal";
 
-export default function InventoryModal ({ isOpen, onClose,inventoryId, fetchData }) {
+export default function InventoryModal({
+  isOpen,
+  onClose,
+  inventoryId,
+  fetchData,
+}) {
   const [inventoryName, setInventoryName] = useState("");
- 
+
   useEffect(() => {
     const fetchInventoryName = async () => {
       try {
         if (isOpen && inventoryId) {
-        const response = await axios.get(`http://localhost:8080/inventories/${inventoryId}`);
-        setInventoryName(response.data.item_name);
+          const response = await axios.get(
+            `http://localhost:8080/inventories/${inventoryId}`
+          );
+          setInventoryName(response.data.item_name);
         }
       } catch (error) {
-        console.error('Error fetching inventory name:', error);
+        console.error("Error fetching inventory name:", error);
       }
     };
 
-      fetchInventoryName();
-    
+    fetchInventoryName();
   }, [isOpen, inventoryId]);
 
-  const deleteInventory = async () => {        
+  const deleteInventory = async () => {
     try {
       await axios.delete(`http://localhost:8080/inventories/${inventoryId}`);
       fetchData();
       onClose();
     } catch (error) {
-      console.error('Could not delete inventory:', error);
+      console.error("Could not delete inventory:", error);
     }
-  }
+  };
   const handleDelete = () => {
     deleteInventory(inventoryId);
     onClose();
-  }
-  
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} handleDelete={handleDelete}>
-        <div className='inventory-modal__header'>Delete {inventoryName} inventory</div>
-        <div className='inventory-modal__message'>
-            Please confirm that you'd like to delete the {inventoryName} inventory from the list. You won't be able to undo this action.
-        </div>
+      <h1 className="inventory-modal__header">
+        Delete {inventoryName} inventory
+      </h1>
+      <p className="inventory-modal__message">
+        Please confirm that you'd like to delete {inventoryName} from the
+        inventory list. You won't be able to undo this action.
+      </p>
     </Modal>
   );
-};
+}
