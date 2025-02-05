6cf95b17dd65d67b849c5fcd24bcfea9f35035c7
Vina Quiambao <mariavinaq@gmail.com>
2025-02-05 02:08:59 -0500

    Fix Edit icon path

--- src/pages/InventoryDetailsPage/InventoryDetailsPage.jsx
+++ src/pages/InventoryDetailsPage/InventoryDetailsPage.jsx
@@ -3,8 +3,8 @@ import "./InventoryDetailsPage.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import Edit from "../../assets/icons/edit-white-24px.svg";
-import BackArrow from "../../assets/icons/arrow_back-24px.svg";
+import Edit from "../../assets/Icons/edit-white-24px.svg";
+import BackArrow from "../../assets/Icons/arrow_back-24px.svg";
 import InventoryItemDetails from "../../components/InventoryItemDetails/InventoryItemDetails";
 import API_URL from "../../utils/utils";
 
